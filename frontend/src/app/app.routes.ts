bc21c973ea0648868a86ab03cbc12fe1b3ba98f9
jashvanth370 <jashjazz11@gmail.com>
2026-03-12 12:22:39 +0530

    guard , interceptor and routes files created and updated

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', loadComponent: () => import('./components/login/login').then(m => m.Login) },
+  { path: 'register', loadComponent: () => import('./components/register/register').then(m => m.Register) },
+  { path: 'books', loadComponent: () => import('./components/book-list/book-list').then(m => m.BookList), canActivate: [authGuard] },
+  { path: 'books/add', loadComponent: () => import('./components/book-form/book-form').then(m => m.BookForm), canActivate: [authGuard] },
+  { path: 'books/edit/:id', loadComponent: () => import('./components/edit-book/edit-book').then(m => m.EditBook), canActivate: [authGuard] },
+  { path: '', redirectTo: 'books', pathMatch: 'full' }
+];
